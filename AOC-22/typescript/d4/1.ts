3bd72263b81bbad5415a3182954b8c68e5858154
Ashley Thompson <workashleythompson@gmail.com>
2022-12-09 20:37:36 +1100

    refactor part 1

--- AOC-22/typescript/d4/1.ts
+++ AOC-22/typescript/d4/1.ts
@@ -3,22 +3,22 @@ import fs from 'fs'
 const input = fs.readFileSync(__dirname + '/input.txt', 'utf-8')
 const inputArray = input.split(/\n/)
 
+function splitSection(section: string): number[] {
+  return section.split('-').map((n) => Number(n))
+}
+
 const pairs = inputArray.map((pair) => {
   const a = pair.split(',')
 
-  const sections = a.map((section) => section.split('-').map((n) => Number(n)))
+  const sections = a.map((section) => splitSection(section))
   console.log(sections)
   return sections
 })
 
 let overlapCount = 0
 
-// check how many pairs have sections that are fully contained within the other
-
-// [start, end], [start, end]
-// if start[0] >= start[1] && end[0] <= end[1]
-
 for (let pair of pairs) {
+  // [start, end], [start, end]
   const [p1, p2] = pair
 
   if (
