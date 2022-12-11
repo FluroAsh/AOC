1d071bc130f1fb271625d9fc3f23078eff701cb8
Ashley Thompson <workashleythompson@gmail.com>
2022-12-11 19:25:07 +1100

    completes d5 part 1 & 2

---
+++ AOC-22/typescript/d5/index.ts
@@ -0,0 +1,103 @@
+import fs from 'fs'
+import { IStacks, IMoves, ITopCrates } from './index.types'
+
+// 🥴 Input Parsing Madness...
+const inputs = fs.readFileSync(__dirname + '/input.txt', 'utf8')
+
+const [rawStacks, rawMoveSet] = inputs
+  .split('\n\n') // seperate stacks from moves
+  .map((lines) => lines.split('\n'))
+
+// transform by removing spaces/square brackets into something that looks like a grid
+const parsedStacks = rawStacks.map((line) =>
+  [...line].filter((_, i) => i % 4 === 1)
+)
+
+const indexes = parsedStacks.pop()
+const stacks: IStacks = {}
+
+// 🏗️ Creates our stack structure
+for (let line of parsedStacks) {
+  for (let i = 0; i < line.length; i++) {
+    if (line[i] !== ' ') {
+      if (!indexes) {
+        continue // avoids type errors - pop 'can' return undefined
+      }
+
+      if (!stacks[indexes[i]]) {
+        stacks[indexes[i]] = []
+      }
+      stacks[indexes[i]].unshift(line[i])
+    }
+  }
+}
+
+const parsedMoves: IMoves[] = []
+
+for (let move of rawMoveSet) {
+  const match = move.match(/\d+/g)
+
+  if (!match) {
+    continue // again, avoids type errors - same scenario as pop
+  }
+
+  parsedMoves.push({
+    count: parseInt(match[0]),
+    from: parseInt(match[1]),
+    to: parseInt(match[2])
+  })
+}
+
+function executeMoves(
+  localStacks: IStacks,
+  moveSet: IMoves,
+  keepOrder?: boolean
+): void {
+  const { count, from, to } = moveSet
+
+  if (keepOrder) {
+    const crates = localStacks[from].splice(-count, count) // No. of crates to 'pick up' (keeping original order)
+    localStacks[to] = localStacks[to].concat(crates) // adds to the END of the array...
+  }
+
+  if (!keepOrder) {
+    for (let i = 0; i < count; i++) {
+      let crate = localStacks[from].pop()
+      if (crate) {
+        localStacks[to].push(crate)
+      }
+    }
+  }
+}
+
+function partOne(): ITopCrates {
+  // FYI: Deep copy for when we execute Part Two - prevents mutation of original stacks object
+  const localStacks: IStacks = JSON.parse(JSON.stringify(stacks))
+
+  for (let moveSet of parsedMoves) {
+    executeMoves(localStacks, moveSet)
+  }
+
+  return {
+    PartOne: Object.values(localStacks)
+      .map((column) => column.slice(-1))
+      .join('')
+  }
+}
+
+function partTwo(): ITopCrates {
+  const localStacks: IStacks = JSON.parse(JSON.stringify(stacks))
+  for (let moveSet of parsedMoves) {
+    executeMoves(localStacks, moveSet, true)
+  }
+
+  return {
+    PartTwo: Object.values(localStacks)
+      .map((column) => column.slice(-1))
+      .join('')
+  }
+}
+
+console.log()
+console.log(partOne())
+console.log(partTwo())
---
+++ AOC-22/typescript/d5/index.types.ts
@@ -0,0 +1,13 @@
+export interface IStacks {
+  [key: string]: string[]
+}
+
+export interface IMoves {
+  count: number
+  from: number
+  to: number
+}
+
+export interface ITopCrates {
+  [key: string]: string
+}
