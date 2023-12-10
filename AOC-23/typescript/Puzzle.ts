e135530a71cc107bd1b98bc5fe1ad2973977a69f
Ashley Thompson <workashleythompson@gmail.com>
2023-12-11 01:43:36 +1100

    (2023): simplify d2-p1 solution & adds reusable "Puzzle" class

---
+++ AOC-23/typescript/Puzzle.ts
@@ -0,0 +1,22 @@
+const partMap = {
+  partOne: 'Part One',
+  partTwo: 'Part Two'
+} as const
+
+export default class Puzzle {
+  public input: string
+  public answers: { [part: string]: string | number }
+
+  constructor(input) {
+    this.input = input
+    this.answers = {}
+  }
+
+  solve(fn: () => string | number) {
+    const fnName = fn.name
+    const partName = partMap[fnName]
+
+    console.log(`> Solving for "${partName}"`)
+    this.answers[partName] = fn()
+  }
+}
--- AOC-23/typescript/d2/index.ts
+++ AOC-23/typescript/d2/index.ts
@@ -1,46 +1,43 @@
 import { readInput } from '@/utils'
+import Puzzle from '../Puzzle'
 console.clear()
 
 const input = readInput(import.meta.url)
+const day2 = new Puzzle(input)
 const lines = input.split('\n')
 
-type GameData = {
-  [gameId: string]: Array<string>
-}
-
-// Answer = Return games that can only be possible given the bag contains (should be the sum of IDs):
-// "12 red", "13 green", "14 blue"
+type GameData = { [gameId: string]: string[][] }
 
 const config = { red: 12, green: 13, blue: 14 }
 
-let possibleGames: number[] = []
-console.log('> [OBJECTIVE]: Must be possible with "12 red", "13 green" and "14 blue"')
+const parsedData: GameData = lines.reduce((acc, line) => {
+  const [gameString, ...sets] = line.split(/[:;]\s+/)
+  const gameNumber = gameString.split(/\s+/)[1] // extract the number of the game from the string
+  const parsedSets = sets.map((subset) => subset.split(/,\s+/)) // separate each color "pick" in the array
 
-// for each line (game)
-lines.forEach((line) => {
-  let isPossible = true
+  acc[gameNumber] = parsedSets
+  return acc
+}, {})
 
-  const data = line.split(/[:;]/).map((line) => line.trim())
-  const [gameString, ...sets] = data
-  const gameNumber = gameString.replace(/Game\s+/, '') // extract the number of the game from the string
-
-  sets.forEach((subset) => {
-    const handful = subset.split(/,\s+/) // Separate colors from each other
-
-    // Checking for each handful if we have any colors that exceed the
-    // color limit (in config) for the subset
-    handful.forEach((colorData) => {
-      const [colorCount, color] = colorData.split(' ')
-
-      if (Number(colorCount) > config[color]) {
-        isPossible = false
-      }
+function partOne() {
+  // Check if the color picks contain any INVALID values based on our "config" Object
+  const validIds = Object.entries(parsedData).map(([gameNumber, subsets]) => {
+    // search each subset to determine if the set is valid (doesn't break config rules)
+    const validSetValues = subsets.map((subset) => {
+      const validSubsets = subset.every((colorPick) => {
+        const [colorCount, colorName] = colorPick.split(' ')
+        return Number(colorCount) <= config[colorName]
+      })
+      return validSubsets
     })
+
+    const validSet = validSetValues.every((e) => e === true) // Check if the set is valid based on if EVERY subset is valid
+    return validSet ? parseInt(gameNumber) : null
   })
 
-  if (isPossible) possibleGames.push(parseInt(gameNumber))
-})
+  const answer = validIds.reduce((a, b) => a + b, 0)
+  return answer
+}
 
-const sum = possibleGames.reduce((a, b) => a + b, 0)
-console.log({ possibleGames, sum })
-// 2545
+day2.solve(partOne) // 2545
+console.log(day2.answers)
