7bf480bb0d869bdc12b9a447b13aa7ffa6acaf86
Ashley Thompson <workashleythompson@gmail.com>
2023-12-14 00:58:51 +1100

    (2023-d3/p1): Completes Part 1

--- AOC-23/typescript/d3/index.ts
+++ AOC-23/typescript/d3/index.ts
@@ -7,80 +7,72 @@ const day3 = new Puzzle(input)
 
 const lines = input.split('\n')
 
-// index of the line
-// starting end of the number, end index of the number
-// index of the symbol
-// > compare the index of the symbol to the start & end index of the number
-// > if the difference is greater than one then don't include in the sum...
+const isSymbol = (char: string) => /[@#$%&*\/+\-=]/.test(char)
+const isDigit = (char: string) => /[0-9]/.test(char)
+
+type SymbolCoordinates = [{ matchedString: string; row: number; col: number }]
 
 function getSymbolCoordinates() {
   const symbolStorage = []
 
   lines.forEach(([...chars], i) => {
     chars.forEach((char, k) => {
-      const symbolRegex = /[@#$%&*\/+\-=]/
-
-      const symbolMatch = symbolRegex.test(char)
-      if (symbolMatch) {
-        console.log(char, i, k) // NOTE: Logging symbol matches
-        symbolStorage.push({ matchedString: char, row: i + 1, col: k })
+      if (isSymbol(char)) {
+        symbolStorage.push({ matchedString: char, row: i, col: k })
       }
     })
   })
-  console.log(symbolStorage)
-  return symbolStorage
+  return symbolStorage as SymbolCoordinates
 }
 
 function partOne() {
-  // find indexes of symbols
-  // 1. For every symbol, check the previous element and next element for a number
-  // 2. If that element is no more than 1 from the symbol index (eg: Symbol index 2 & Element number index is )
-
-  // current row
-  // check previous (-1) and next (+1)
+  // NOTE:
+  // For every number in a line, get the first and last number (as-well as index)
+  // Then check if it overlaps with a symbol on the same, previous or following row
   const symbolCoordinates = getSymbolCoordinates()
-  const numberMatches = []
-  const adjacentNumbers = []
+  const matches = []
 
-  lines.forEach((chars, i) => {
-    const regex = /\d{0,}/g
-    const matches = chars.matchAll(regex)
+  lines.forEach(([...chars], charRow) => {
+    let numSequence = []
+    let foundMatch = false
 
-    for (const match of matches) {
-      const matchedString = match[0]
-      const lastIndex = match.index + matchedString.length - 1
+    chars.forEach((char, charCol) => {
+      const hasNums = numSequence.length > 0
+      const endOfLine = chars[charCol + 1] === undefined
+      const shouldPushMatch = hasNums && foundMatch && (char === '.' || isSymbol(char) || endOfLine)
 
-      if (matchedString) {
-        numberMatches.push({ matchedString, row: i + 1, startCol: match.index, endCol: lastIndex })
+      if (shouldPushMatch) {
+        if (endOfLine) numSequence.push(char)
+        // console.log('> Pushing', matchedNums, { char, matchFound: foundMatch })
+        matches.push(numSequence.join(''))
+        foundMatch = false
+        numSequence = []
       }
-    }
-  })
 
-  console.log(numberMatches)
-  numberMatches.forEach(({ matchedString: number, row: numberRow, startCol, endCol }) => {
-    symbolCoordinates.forEach(({ matchedString: _symbol, row: symbolRow, col: symbolCol }) => {
-      // same row, previous row or next row
-      const maxRow = symbolRow + 1
-      const minRow = symbolRow - 1 < 0 ? symbolRow : symbolRow - 1
-      const inRange = symbolRow === numberRow || numberRow === maxRow || numberRow === minRow
+      if (isDigit(char)) {
+        numSequence.push(char)
+        // console.log('> Adding num >>', matchedNums) // when digit is found
+      } else {
+        numSequence = [] // Clear saved numbers if it's a Symbol or "."
+      }
 
-      // check if number row is range of the symbol (-1, 0, +1)
-      if (inRange) {
-        // console.log(`${number} found in row range. Symbol = ${symbol}`)
-        const start = symbolCol === startCol || symbolCol === startCol - 1
-        const end = symbolCol === endCol || symbolCol === endCol + 1
-        const isNeighbour = start || end
-        // console.log({ isNeighbour, symbolCol, startCol, endCol, start, end })
+      // Checking that the character row is located within 1 space of the Symbol in any direction
+      const charIsNeighbour = symbolCoordinates.find(
+        (symbol) =>
+          (symbol.row === charRow || symbol.row === charRow - 1 || symbol.row === charRow + 1) &&
+          (symbol.col === charCol || symbol.col === charCol - 1 || symbol.col === charCol + 1)
+      )
 
-        if (isNeighbour) {
-          adjacentNumbers.push(number)
-        }
+      if (charIsNeighbour && !isSymbol(char) && char !== '.') {
+        // console.log(' >> ✅ Match', char)
+        foundMatch = true
       }
     })
   })
-  console.log(adjacentNumbers)
-  return adjacentNumbers.reduce((a, b) => a + Number(b), 0)
+
+  console.log('matches', matches)
+  return matches.reduce((a, b) => a + Number(b), 0)
 }
 
-day3.solve(partOne) // 441902 (too low) - (answer = 527144)
+day3.solve(partOne) // 527144
 console.log(day3.answers)
